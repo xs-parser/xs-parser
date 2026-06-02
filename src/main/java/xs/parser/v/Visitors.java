dd81d7f6c53a43139055b45501ea75d44b0f207d
Thomas Bateson <tbateson@users.noreply.github.com>
2026-06-02 09:30:41 -0500

    Add node helper deep clone node

--- src/main/java/xs/parser/v/Visitors.java
+++ src/main/java/xs/parser/v/Visitors.java
@@ -1,6 +1,7 @@
 package xs.parser.v;
 
 import java.util.*;
+import org.w3c.dom.*;
 import xs.parser.*;
 import xs.parser.internal.util.*;
 
@@ -21,6 +22,30 @@ public final class Visitors {
 	public static void visit(final SchemaComponent schemaComponent, final Visitor visitor) {
 		Objects.requireNonNull(visitor, "visitor");
 		VisitorHelper.lookup(schemaComponent.getClass()).accept(schemaComponent, visitor);
+	}
+
+	/**
+	 * Returns a deep clone of the node backing the provided schema component.
+	 * Note: this method is not a performant implementation and should only be used for rapid prototyping or ad-hoc situational use.
+	 * @param schemaComponent The schema component
+	 * @return a deep clone of the node backing the provided schema component
+	 */
+	public static Node deepCloneNode(final SchemaComponent schemaComponent) {
+		final class CloneNodeVisitor extends DefaultVisitor {
+
+			private Node node;
+
+			@Override
+			public boolean visit(final AnnotatedComponent context, final Node node, final SchemaComponent s) {
+				this.node = node;
+				return false;
+			}
+
+		}
+
+		final CloneNodeVisitor cloneNodeVisitor = new CloneNodeVisitor();
+		visit(schemaComponent, cloneNodeVisitor);
+		return cloneNodeVisitor.node.cloneNode(true);
 	}
 
 }
--- src/test/java/xs/parser/v/VisitorTests.java
+++ src/test/java/xs/parser/v/VisitorTests.java
@@ -1,7 +1,10 @@
 package xs.parser.v;
 
+import java.io.*;
 import java.util.*;
 import org.junit.*;
+import org.w3c.dom.*;
+import org.w3c.dom.Element;
 import xs.parser.*;
 import xs.parser.internal.util.*;
 
@@ -22,6 +25,20 @@ public class VisitorTests {
 
 		};
 		Assert.assertThrows(IllegalArgumentException.class, () -> Visitors.visit(a, visitor));
+	}
+
+	@Test
+	public void testDeepCloneNode() throws Exception {
+		final Schema schema = new Schema(new File("src/test/resources/schema/base.xsd"));
+		final Node schemaNode = Visitors.deepCloneNode(schema);
+		Assert.assertNotNull(schemaNode);
+		Assert.assertTrue(schemaNode instanceof Document);
+		Arrays.asList(schema.attributeDeclarations(), schema.attributeGroupDefinitions(), schema.elementDeclarations(), schema.identityConstraintDefinitions(), schema.modelGroupDefinitions(), schema.annotations(), schema.typeDefinitions()).forEach(a ->
+				a.forEach(d -> {
+					final Node node = Visitors.deepCloneNode(d);
+					Assert.assertNotNull(node);
+					Assert.assertTrue(node instanceof Element);
+				}));
 	}
 
 }
